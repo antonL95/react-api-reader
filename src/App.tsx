d06e545411bd7e7afb0f0e151cd6df1798c1a680
Anton Loginov <loginovanton95@gmail.com>
2021-02-08 12:50:18 +0100

    Init commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    interface widgetProp {
+        offer: any
+        merchant: any
+        id: number
+        image: string
+        shipping: any
+    }
+
+    let [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://search-api.fie.future.net.uk/widget.php?id=review&model_name=xbox_one_s&area=GB").then(response => setData(response.data.widget.data.offers))
+    }, []);
+
+    const renderImage = (image:string, name: string) => {
+        if (image !== null) {
+            return (
+                <div>
+                    <img src={image} alt={name}/>
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                Image is not available
+            </div>
+        )
+    }
+
+
+    const renderRows = () => {
+        return data.map((item: widgetProp) => {
+            return (
+                <div key={item.id}>
+                    {
+                        renderImage(item.image, item.offer.name)
+                    }
+                    <div>
+                        {item.offer.name}
+                    </div>
+                    <div>
+                        {item.offer.price} <span dangerouslySetInnerHTML={{__html: `${item.offer.currency_symbol}`}}/>
+                    </div>
+                    {item.shipping.prime && (
+                        <div><a href={item.shipping.url} target="_blank">Prime shipping</a></div>
+                    )}
+                    <div>
+                        <a href={item.offer.link} target="_blank">
+                            <button className="btn btn-primary">
+                                Buy on {item.merchant.name}
+                            </button>
+                        </a>
+                    </div>
+                    <div>
+                        <a href={item.offer.link}><img src={item.merchant.logo_url} alt={item.merchant.name}/></a>
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    return (
+        <div className="offers">
+            {renderRows()}
+        </div>
+    );
 }
 
 export default App;
